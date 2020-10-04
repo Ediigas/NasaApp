d90e16c83b294e5240b84ba08fd6e3914ced3209
Ediigas <douglas.edig@gmail.com>
2020-10-04 18:39:00 -0300

    tela de carregamento

---
+++ src/assets/Telaone.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Image,
+
+} from 'react-native';
+
+export default function App() {
+  return (
+
+    <View>
+      <Image
+      source={require('./src/assets/telaone.jpg')}
+      />
+    </View>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+
+
+})
